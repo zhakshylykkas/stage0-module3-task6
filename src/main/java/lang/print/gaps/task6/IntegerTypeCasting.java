0dd70dc38076b5aee90f7038e1fc05c2abafd823
Zhakshylyk Kasymbekov <jakkasymbekov@gmail.com>
2023-09-01 20:08:31 +0600

    IntegerTypeCasting task commit after the second testing

--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -3,8 +3,7 @@ package lang.print.gaps.task6;
 public class IntegerTypeCasting {
     public static void main(String[] args) {
         long first = 1000L;
-        int second = 1000;
-        first = second;
+        int second = (int) first;
 
         System.out.println(first);
         System.out.println(second);
